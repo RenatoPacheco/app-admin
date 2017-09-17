59533690f883711bf129045543e648509c4cbafc
Renato Pacheco <renato2k@hotmail.com>
2017-09-17 11:29:26 -0300

    Configurando para que curso altera e detalhe possa obter os dados da url.

--- src/app/pages/curso/altera/altera.page.ts
+++ src/app/pages/curso/altera/altera.page.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'page-altera',
   templateUrl: './altera.page.html',
   styleUrls: ['./altera.page.less']
 })
-export class AlteraPage implements OnInit {
+export class AlteraPage implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(
+    private _route: ActivatedRoute
+  ) { }
 
-  ngOnInit() {
+  public id: string;
+  private _inscricao: Subscription;
+
+  public ngOnInit(): void {
+    this._inscricao = this._route.params.subscribe((params: any) => {
+      this.id = params['id'];
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this._inscricao.unsubscribe();
   }
 
 }
